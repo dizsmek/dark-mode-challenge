e7e48fa1502c26484b08c4a7b8853c260b62dc56
Miklos Ballo <dizsmek@gmail.com>
2020-12-15 02:46:22 +0100

    solution with context created

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import ThemeContext from './contexts/ThemeContext';
 
 export default class AppContainer extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            theme: 'light',
+            toggleTheme: this.toggleTheme
+        }
+    }
+
+    toggleTheme = () => {
+        this.setState(prevState => ({ theme: prevState.theme === 'light' ? 'dark' : 'light' }))
+    }
 
     render() {
         const {children} = this.props;
+        const darkMode = this.state.theme === 'dark';
 
         return (
-            <div className="app-container">
-                {children}
-            </div>
+            <ThemeContext.Provider value={this.state}>
+                <div className={`app-container${darkMode ? ' dark-mode' : ''}`}>
+                    {children}
+                </div>
+            </ThemeContext.Provider>
         );
     }
 
--- src/partials/ThemeSwitcher.js
+++ src/partials/ThemeSwitcher.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import ThemeContext from '../components/contexts/ThemeContext';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMoon} from '@fortawesome/free-solid-svg-icons';
+import {faMoon, faSun} from '@fortawesome/free-solid-svg-icons';
 
 export default class ThemeSwitcher extends React.Component {
+    static contextType = ThemeContext;
 
     render() {
+        const { theme, toggleTheme } = this.context;
+        const darkMode = theme === 'dark';
+
         return (
-            <button className="app__dark-mode-btn icon level-right">
-                <FontAwesomeIcon icon={faMoon} color={'#4D5B6B'}/>
+            <button className="app__dark-mode-btn icon level-right" onClick={toggleTheme}>
+                <FontAwesomeIcon icon={darkMode ? faSun : faMoon} color={darkMode ? '#FFA500' : '#4D5B6B'}/>
             </button>
         );
     }
